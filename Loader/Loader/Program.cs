80a6717af2d818f24dbeb662b6fd15ff271eff08
ameyer15801 <username@users.noreply.github.com>
2020-09-03 17:06:05 -0500

    Loader.csproj - use Console

--- Loader/Loader/Program.cs
+++ Loader/Loader/Program.cs
@@ -1,48 +1,59 @@
-﻿using System;
+﻿using Meyer.Common.Console;
+using System;
 using System.Diagnostics;
 
 namespace Meyer.BallChasing.Loader
 {
     class Program
     {
+        private static Aliases alias;
+
+        private static ConsoleParameters consoleParameters = new ConsoleParameters
+        {
+            OrderedConsoleParameters =
+            {
+                new OrderedEnumConsoleParameter<Aliases>(() => alias, "Specify one of the actions")
+            }
+        };
+
         static void Main(string[] args)
         {
             try
             {
-                Aliases alias = Enum.Parse<Aliases>(args[0], true);
-                switch (alias)
-                {
-                    case Aliases.push:
-                        args[0] = "Meyer.BallChasing.Push.dll";
-                        break;
-                    case Aliases.stats:
-                        args[0] = "Meyer.BallChasing.PullStats.dll";
-                        break;
-                }
+                consoleParameters.Map(args, true);
+            }
+            catch (Exception e)
+            {
 
-                Process.Start(new ProcessStartInfo
-                {
-                    FileName = "dotnet",
-                    Arguments = string.Join(" ", args),
-                    WorkingDirectory = Environment.CurrentDirectory
-                })
-                .WaitForExit();
-            }
-            catch (IndexOutOfRangeException)
-            {
-                Console.WriteLine($"Please specify one of the actions: {string.Join(", ", Enum.GetNames(typeof(Aliases)))} along with required arguments");
+                Console.WriteLine(e.Message);
+                Console.WriteLine(consoleParameters.ToString());
+
                 Environment.Exit(-1);
             }
-            catch (ArgumentException)
+
+            switch (alias)
             {
-                Console.WriteLine($"Please specify one of the actions: {string.Join(", ", Enum.GetNames(typeof(Aliases)))} along with required arguments");
-                Environment.Exit(-1);
+                case Aliases.push:
+                    args[0] = "Meyer.BallChasing.Push.dll";
+                    break;
+                case Aliases.stats:
+                    args[0] = "Meyer.BallChasing.PullStats.dll";
+                    break;
             }
+
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "dotnet",
+                Arguments = string.Join(" ", args),
+                WorkingDirectory = Environment.CurrentDirectory
+            })
+            .WaitForExit();
         }
     }
 
     enum Aliases
     {
+        @null,
         push,
         stats
     }
